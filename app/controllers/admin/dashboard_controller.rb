9fa98b1c1f602cab944455171ab3fa794cbe2c12
Cameron <cameron_uniac@hotmail.com>
2021-08-19 01:26:37 -0400

    Add shop stats to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 
   def show
+    @categories_count = Category.all.count
+    @products_count = Product.all.count
   end
 end
