b82b8e20c530e9f2116d060f4f479f22e766385c
Cameron <cameron_uniac@hotmail.com>
2021-08-19 20:26:14 -0400

    Fix bcrypt error by updating to 3.1.16

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def create
+    puts "USER PARAMS: #{user_params}"
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
@@ -12,7 +13,9 @@ class UsersController < ApplicationController
       redirect_to '/signup'
     end
   end
-
+  
+  private
+  
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
