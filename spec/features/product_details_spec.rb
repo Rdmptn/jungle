5e4b49c5a94203ad14cea7c4b31adc011402dc63
Cameron <cameron_uniac@hotmail.com>
2021-08-25 22:09:37 -0400

    Add working test for product detail page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "ProductDetails", type: :feature, js: true do
+  
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+  
+  scenario "You can click on a product and view the details" do
+    # ACT
+    visit root_path
+    click_on 'Details', match: :first
+    
+    # VERIFY
+    expect(page).to have_css 'article.product-detail'
+    save_screenshot
+  end
+    
+end
